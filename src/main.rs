3802c190513d5dbe4e3da0c53719b5052f851fcd
Shakib448 <shakiba448@gmail.com>
2024-12-30 14:54:43 +0600

    RUST JSON WRITER

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{Parser};
-
+use std::{fs, path::Path};
+use serde_json::Value;
 
 #[derive(Parser)]
 #[command(
@@ -18,8 +19,28 @@ struct Cli {
 }
 
 
-fn main() {
+fn main()  -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    println!("Hello, world!");
+    let output_dir = "output";
+    let output_file_path = format!("{}/{}", output_dir, cli.filename);
+
+    if !Path::new(output_dir).exists() {
+        fs::create_dir_all(output_dir)?;
+    }
+
+    let response = reqwest::blocking::get(&cli.url)?;
+    let json_data: Value = response.json()?;
+
+    let json_string = serde_json::to_string_pretty(&json_data)?;
+
+    fs::write(&output_file_path, json_string)?;
+
+    println!(
+        "Fetched JSON data from {} and wrote it to: {}",
+        cli.url, output_file_path
+    );
+
+
+    Ok(())
 }
